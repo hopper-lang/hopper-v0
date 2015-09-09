33deef27ed580ecc6344d99b7597d2c909b34c8a
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-09-09 17:53:22 -0400

    starting the transaction language

---
+++ src/Language/Lambomba/Internal/Core.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+
+module Language.Lambomba.Internal.Core where
+
+
+import Bound
+import Numeric.Natural
