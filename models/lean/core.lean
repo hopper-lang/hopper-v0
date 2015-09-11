1dc5c8499d8175fb5351bf1456afbb10f7b4ed52
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-09-11 10:30:45 -0400

    starting the works

---
+++ models/lean/core.lean
@@ -0,0 +1,42 @@
+import data.bool 
+import data.nat
+open nat
+check nat 
+
+/-
+debrujin representation 
+-/
+inductive HasImpred := | Impred : HasImpred | Pred : HasImpred 
+
+set_option pp.all true 
+open HasImpred 
+
+definition meet : HasImpred -> HasImpred -> HasImpred
+| meet Pred Pred := Pred 
+| meet Impred Impred := Impred
+| meet Pred Impred := Impred
+| meet Impred Pred := Impred 
+check ∀ (a b  : Type) , a -> b 
+check Type.{imax  1 0}
+check ∀ {t1} , ∀ {t2 : t1}, (t1 : Prop)
+check ∀ {t1 f} , ∀ {t2 : t1}, f t2 
+check ∀ {t1 f} , ∀ {t2 : t1}, t1 -> f t2
+check Type.{1} -> Type.{2}
+
+definition zeroOrPlusMax : nat -> nat -> nat 
+|zeroOrPlusMax 0 0 := 0 
+|zeroOrPlusMax n m :=  1 +  (max n m )
+
+inductive type :  nat-> HasImpred  -> Type := 
+  | lift : ∀ {n} , type  n Pred -> type  (succ n) Pred
+  | var : ∀ {n hasimp} , nat -> type  n hasimp 
+  | tyBool : ∀ {n} ,type n Pred 
+  | tyInteger : ∀ {n}, type n Pred 
+  | typString : ∀ {n}, type n Pred 
+  | impredUniversal : ∀ { hasimpA hasimpB},type  0 hasimpA ->type 0 hasimpB  -> type  0 Impred
+ -- i do want impred, but will ignore for now
+  | predUniversal : ∀ {n m},type  n Pred -> type m Pred -> type  (zeroOrPlusMax n m )  Pred 
+--   | predarr : ∀ {n m  hasImpA hasImpB}, type n Pred -> type m  Pred -> type n (meet hasImpA hasImpB)
+  with term  : ∀ {n himp}, type n himp -> Type :=
+  | tmBool : ∀ {n himp}, bool -> term (@tyBool n) 
+ 
--- src/Language/Lambomba/Internal/Core.hs
+++ src/Language/Lambomba/Internal/Core.hs
@@ -8,3 +8,15 @@ module Language.Lambomba.Internal.Core where
 
 import Bound
 import Numeric.Natural
+
+
+{- |  this iteration is essentially F_\omega, plus linear types,
+plus inductive data types obeying the positivity condition, plus "crypto signed" values,
+plus .... damnit, need dependent types to sanely treat crypto signed values
+
+why?
+because we dont know the commit id that a signed value in the result of a higher order transaction refers to until the parent computation is commited!
+-}
+
+
+
