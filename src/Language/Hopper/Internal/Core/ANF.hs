802e70dc2367da332ca9e83b7d9b589dd84d0e0d
Carter T Schonwald <carter.schonwald@gmail.com>
2015-11-09 16:03:43 -0500

    WIP crazy sauce

--- src/Language/Hopper/Internal/Core/ANF.hs
+++ src/Language/Hopper/Internal/Core/ANF.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable,DeriveAnyClass #-}
 -- {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 -- {-# LANGUAGE KindSignatures #-}
 -- {-# LANGUAGE DeriveGeneric #-}
 
@@ -17,13 +18,16 @@ import Data.Data
 import  Control.Monad
 import Prelude.Extras
 import Bound
+-- import Language.Hopper.Internal.Core.Value (Tag)
 
-data Atom  ty a =
-    AtomVar !a
-    | AtomicLit !Literal
-    | AtomLam ![(Text,Type ty,RigModel)] -- do we want to allow arity == 0, or just >= 1?
-                !(Scope Text (ANF ty)  a)
-   deriving(Eq,Ord,Functor,Foldable,Traversable,Data,Show,Read)
+newtype Atom  ty a = AtomVar {unVar :: a}
+
+    -- | AtomicLit !Literal
+  -- lambdas need a heap allocation to do the closure allocation in general
+  -- even if they are on the left hand side of an application, UNLESS they are fully applied at that use site
+  --  | AtomLam ![(Text,Type ty,RigModel)] -- do we want to allow arity == 0, or just >= 1?
+  --             !(Scope Text (ANF ty)  a)
+   deriving(Eq,Ord,Functor,Foldable,Traversable,Data,Show,Read,Typeable)
 
 instance Eq ty => Eq1 (Atom ty)
 instance Show ty => Show1 (Atom ty)
@@ -36,19 +40,60 @@ instance Ord2 Atom
 instance Read2 Atom
 
 
---data AnfRHS ty a = PrimApp | FunApp |
+-- at runtime 'ConstrId' is mapped to a tag???
+newtype ConstrId = ConstrId { unConstrId :: Text } deriving (Eq, Show,Data,Typeable,Ord,Read)
 
-data ArgANF ty a = ArgVar a | ArgLit !Literal
+-- | the right hand side of a let, aka 'AnfRHS' is the point where heap allocation of thunks happens
+-- the only other
+data AnfRHS ty a = PrimApp !Text ![Atom ty a]
+                 | FunApp a [Atom ty a]
+                 | ConstrApp !ty ConstrId [Atom ty a]
+                 | SharedLiteral !Literal -- we currently do not have any fixed size literal types
+                                          -- so for now all literals are heap allocated
+                                          -- this will change once we add support for stuff like
+                                          -- Double or Word64
+                 | AllocateThunk !(ANF ty a) -- Thunks share their evaluations
+                 | EvaluateThunk !a       -- Thunk evaluation is a special
+                                          -- no arg lambda plus sharing
+
+                 | AllocateClosure ![(Text,Type ty,RigModel)] -- arity >=0
+                                   !(Scope Text (ANF ty)  a)  -- should we have global table of
+                                                              -- "pointers" to lambdas? THINK ME + FIX ME
+                  {- AllocateClosure {env :: [(Text,a)], codeBod :: CodeIdentifier } ???  -}
+
+   deriving ( Ord,
+    Functor,
+    Foldable,
+    Traversable,
+    Typeable,
+    Data,
+    Eq,
+    Read,
+    Show)
+instance Eq ty => Eq1 (AnfRHS ty)
+instance Show ty => Show1 (AnfRHS ty)
+instance Ord ty => Ord1 (AnfRHS ty)
+instance Read ty => Read1 (AnfRHS ty)
+
+instance Eq2 AnfRHS
+instance Show2 AnfRHS
+instance Ord2 AnfRHS
+instance Read2 AnfRHS
+-- data ArgANF ty a = ArgVar a | ArgLit !Literal
+
 
 data ANF ty a
     = ReturnNF !(Atom ty a)
-    -- | ELitNF !Literal
-    | ForceNF !a !a
-    | !(Atom  ty a) :@@ !(Atom  ty a)
-    | LetApp (Atom ty a) (Atom ty a) (Scope () (ANF ty) a)
+    -- | !(Atom  ty a) :@@ ![(Atom  ty a)]
+    | Let  !(AnfRHS ty a) (Scope () (ANF ty) a)
+    -- future thing will have | LetRec maybe?
 
     -- |
-   deriving ( Ord, Functor, Foldable, Traversable,
+   deriving ( Ord,
+    Functor,
+    Foldable,
+    Traversable,
+    Typeable,
     Data,
     Eq,
     Read,
@@ -63,15 +108,29 @@ instance Show2 ANF
 instance Ord2 ANF
 instance Read2 ANF
 
+withinANF :: (ANF ty a) -> (ANF ty a -> ANF ty a) -> ANF ty a
+withinANF
+
 
 instance Applicative (ANF ty) where
   pure  = \x -> ReturnNF (AtomVar x)
   (<*>) = ap
 
 instance Monad (ANF ty) where
-  (ReturnNF (AtomVar a)) >>=f = f a
-  (ReturnNF (AtomicLit l)) >>= _f = ReturnNF $ AtomicLit l
-  (ReturnNF (AtomLam bs bod)) >>= f = ReturnNF $ AtomLam bs (bod >>>= f)
+  (ReturnNF (AtomVar a)) >>= f = f a
+  (afun :@@ aargs) >>= f =
+        let subst'dArgs :: forall a b .  (a -> ANF ty b) -> [Atom ty a]-> [ANF ty b]
+            subst'dArgs  g  ls = fmap (unVar . fmap g) ls
+            foldrList :: forall a b . (a -> b -> b) -> b -> [a] -> b
+            foldrList = foldr
+            cps'dArgs :: _sdfdsfdsf
+            cps'dArgs = foldrList  cpsStacking id  (subst'dArgs f aargs)
+            -- cpsStacking :: forall  a  . ( ) -> (a -> ANF ty a) -> (a -> ANF ty a)
+            cpsStacking = _lalala
+        in  (f $ unVar $ afun) `cpsStacking` cps'dArgs
+  -- (Let aRHS aBod) >>= f = _dderp
+  -- (ReturnNF (AtomicLit l)) >>= _f = ReturnNF $ AtomicLit l
+  -- (ReturnNF (AtomLam bs bod)) >>= f = ReturnNF $ AtomLam bs (bod >>>= f)
   --( ( ) :@@ )
 
   -- return = V
