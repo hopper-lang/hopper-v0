54294b0de6aecbd482eead832a7722e9c6c5c807
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-11-18 23:41:20 -0500

    type erasure in the value rep

--- src/Language/Hopper/Internal/Core/Value.hs
+++ src/Language/Hopper/Internal/Core/Value.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Language.Hopper.Internal.Core.Value where
 
@@ -32,7 +33,7 @@ import GHC.Generics (Generic)
 
 --import  Control.Monad.Free
 
-import Language.Hopper.Internal.Core.ANF
+--import Language.Hopper.Internal.Core.ANF
 import Language.Hopper.Internal.Core.Literal
 
 
@@ -80,14 +81,14 @@ newtype Tag = Tag { unTag :: Text {-Word64-} } deriving (Eq,Read, Show,Ord,Data,
 -- type ValRec  ty   = Free (ValueF  ty Ref) Ref
 
 
-type HeapVal ast  ty   =  ValueF ast ty Ref --  ValueF ty Ref (ValRec ty)
+type HeapVal ast     =  ValueF ast  Ref --  ValueF ty Ref (ValRec ty)
 
-data ValueF ast ty  v =    VLitF !Literal
+data ValueF ast   v =    VLitF !Literal
               | ConstructorF  !Tag  (WrappedVector v)
-              | ThunkF (ANF  ty v )
+              | ThunkF (ast   v )
               --  should this be a heap ref to
               -- closure to have the right sharing ?
-              | DirectClosureF (Closure ast ty  v) -- heap ref?
+              | DirectClosureF (Closure ast  v) -- heap ref?
               | BlackHoleF
               | IndirectionF v
               --- in the types are calling conventions paper,
@@ -112,6 +113,47 @@ data ValueF ast ty  v =    VLitF !Literal
       --,Show1
       --,Read1
       )
+deriving instance (Data a,Data (ast a),Monad ast,Data (ast (Var Text (ast a))),  Typeable ast,Typeable a)
+  =>  Data (ValueF ast a )
+instance (Eq1 ast,Monad ast) => Eq1 (ValueF ast) where
+    (==#)  (VLitF a) (VLitF b) =  a == b
+    (==#)  (VLitF _) _ =  False
+    (==#)  (ConstructorF tga va) (ConstructorF tgb vb) =   tga == tgb && va == vb
+    (==#)  (ConstructorF _ _) _ =  False
+    (==#)  (ThunkF a) (ThunkF b) =  a ==# b
+    (==#)  (ThunkF _) _ =  False
+    (==#)  (DirectClosureF c1) (DirectClosureF c2) = c1 ==# c2
+    (==#)  (DirectClosureF _ ) _ = False
+    (==#)  BlackHoleF BlackHoleF = True
+    (==#)  BlackHoleF _ =  False
+    (==#) (IndirectionF v) (IndirectionF v2) = v == v2
+    (==#) (IndirectionF _) _ = False
+
+-- this is a trick to make defining the ord1 instance sane but total
+codeValueF :: ValueF a b -> Int
+codeValueF (VLitF _) = 1
+codeValueF (ConstructorF _ _ ) = 2
+codeValueF (ThunkF _) = 3
+codeValueF (DirectClosureF _) = 4
+codeValueF (BlackHoleF) = 5
+codeValueF (IndirectionF _) = 6
+instance (Ord1 ast,Monad ast) => Ord1 (ValueF ast) where
+  compare1 (VLitF a) (VLitF b) = compare a b
+  compare1 a@(VLitF _) b = compare  (codeValueF a) (codeValueF b)
+  compare1 (ConstructorF ta va) (ConstructorF tb vb) =
+        let tcomp = compare ta tb in  if tcomp == EQ then compare va vb else tcomp
+  compare1 a@(ConstructorF _ _) b = compare (codeValueF a) (codeValueF b)
+  compare1 (ThunkF e1) (ThunkF e2) = compare1 e1 e2
+  compare1 a@(ThunkF _) b = compare (codeValueF a) (codeValueF b)
+  compare1 (DirectClosureF a) (DirectClosureF b) = compare1 a b
+  compare1 a@(DirectClosureF _) b = compare (codeValueF a) (codeValueF b)
+  compare1 BlackHoleF BlackHoleF = EQ
+  compare1 a@(BlackHoleF) b = compare (codeValueF a) (codeValueF b)
+  compare1 (IndirectionF a) (IndirectionF b) = compare a b  --- this is spiritually evil :))))
+  compare1 a@(IndirectionF _ ) b = compare (codeValueF a) (codeValueF b)
+
+--- there will NOT be a read1 or show1 in this style
+
 
 newtype WrappedVector a = WrappedVector { unWrappedVector :: V.Vector a }
   deriving (Eq, Show,Ord,Read,Data,Typeable,Functor,Foldable,Traversable)
@@ -127,5 +169,10 @@ data Arity = ArityBoxed {_extractArityInfo :: !Text} --- for now our model of ar
 
 --- | 'Closure' may need some rethinking ... later
 --- they're kinda the erasure of a lambda ... for now
-data Closure ast ty  a = MkClosure ![Arity] !(Scope Text (ast ty) a)
-  deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable,Generic)
+data Closure ast   a = MkClosure ![Arity] !(Scope Text ast a)
+  deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable,Generic,Typeable)
+deriving instance (Typeable ast,Typeable a,Data (Scope Text ast  a))=> Data (Closure ast   a)
+instance (Monad ast, Eq1 ast) => Eq1 (Closure ast)
+instance (Monad ast, Ord1 ast) => Ord1 (Closure ast)
+instance (Monad ast, Read1 ast) => Read1 (Closure ast)
+instance (Monad ast, Show1 ast) => Show1 (Closure ast)
