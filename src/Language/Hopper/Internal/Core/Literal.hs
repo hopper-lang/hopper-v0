bf615ac19020817df9c140beb7343c34fc93ee38
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2016-02-17 16:39:39 -0500

    disabling the non total primops and adding tested with field once we have richer types we can expose the correctness side conditions about avoiding undefined inputs for the currently undefined/commented out operations

--- src/Language/Hopper/Internal/Core/Literal.hs
+++ src/Language/Hopper/Internal/Core/Literal.hs
@@ -6,13 +6,14 @@ module Language.Hopper.Internal.Core.Literal
   ( Literal(..)
   , PrimOpId(..)
   , ConstrId(..)
+  , evalTotalPrimopCode
   ) where
 
 import Numeric.Natural
 import Data.Data
 import Data.Text (Text)
 import GHC.Generics
-import GHC.Integer.GMP.Internals (powModInteger, sqrInteger, lcmInteger,
+import GHC.Integer.GMP.Internals ({-powModInteger,-} sqrInteger, lcmInteger,
                                   gcdInteger, gcdExtInteger, recipModInteger)
 import GHC.Natural (powModNatural)
 
@@ -36,15 +37,15 @@ newtype PrimOpId
   deriving (Eq,Show,Typeable,Ord,Read,Data,Generic)
 
 -- Evaluation
-
+--- currently we only provide the operations which are total for
 data LiteralOp
   = IntAdd      Integer  Integer
   | IntSub      Integer  Integer
   | IntMult     Integer  Integer
   | IntSquare   Integer
-  | IntQuotRem  Integer  Integer
-  | IntDivMod   Integer  Integer
-  | IntPowMod   Integer  Integer  Integer -- a^b mod abs(c)
+  -- | IntQuotRem  Integer  Integer
+  -- | IntDivMod   Integer  Integer
+  -- | IntPowMod   Integer  Integer  Integer -- a^b mod abs(c)
   | IntRecipMod Integer  Integer          -- inv of a mod m. 0 if no inv exists
   | IntLcm      Integer  Integer
   | IntGcd      Integer  Integer
@@ -52,40 +53,40 @@ data LiteralOp
   | RatAdd      Rational Rational
   | RatSub      Rational Rational
   | RatMult     Rational Rational
-  | RatDiv      Rational Rational
+  -- | RatDiv      Rational Rational
   | NatAdd      Natural  Natural
   | NatMult     Natural  Natural
-  | NatQuotRem  Natural  Natural
-  | NatDivMod   Natural  Natural          -- identical to QuotRem for nats
+  -- | NatQuotRem  Natural  Natural
+  -- | NatDivMod   Natural  Natural          -- identical to QuotRem for nats
   | NatPowMod   Natural  Natural  Natural -- a^b mod c
   deriving (Show)
 
 -- NOTE: currently this function is non-total. see comments inline.
 --       these non-total cases need a Maybe or a proof argument.
-eval :: LiteralOp -> [Literal]
-eval (IntAdd      a b) = [LInteger (a + b)]
-eval (IntSub      a b) = [LInteger (a - b)]
-eval (IntMult     a b) = [LInteger (a * b)]
-eval (IntSquare   a)   = [LInteger (sqrInteger a)]
-eval (IntQuotRem  a b) = [LInteger q, LInteger r] -- UNDEFINED for b = 0
-  where (q, r) = a `quotRem` b
-eval (IntDivMod   a b) = [LInteger d, LInteger m] -- UNDEFINED for b = 0
-  where (d, m) = a `divMod` b
-eval (IntPowMod   b e m) = [LInteger (powModInteger b e m)] -- UNDEFINED for some e < 0
-eval (IntRecipMod a b) = [LInteger (recipModInteger a b)]
-eval (IntLcm      a b) = [LInteger (lcmInteger a b)]
-eval (IntGcd      a b) = [LInteger (gcdInteger a b)]
-eval (IntGcdExt   a b) = [LInteger g, LInteger s]
+evalTotalPrimopCode :: LiteralOp -> [Literal]
+evalTotalPrimopCode (IntAdd      a b) = [LInteger (a + b)]
+evalTotalPrimopCode (IntSub      a b) = [LInteger (a - b)]
+evalTotalPrimopCode (IntMult     a b) = [LInteger (a * b)]
+evalTotalPrimopCode (IntSquare   a)   = [LInteger (sqrInteger a)]
+--evalTotalPrimopCode (IntQuotRem  a b) = [LInteger q, LInteger r] -- UNDEFINED for b = 0
+--  where (q, r) = a `quotRem` b
+--evalTotalPrimopCode (IntDivMod   a b) = [LInteger d, LInteger m] -- UNDEFINED for b = 0
+--  where (d, m) = a `divMod` b
+--evalTotalPrimopCode (IntPowMod   b e m) = [LInteger (powModInteger b e m)] -- UNDEFINED for some e < 0
+evalTotalPrimopCode (IntRecipMod a b) = [LInteger (recipModInteger a b)]
+evalTotalPrimopCode (IntLcm      a b) = [LInteger (lcmInteger a b)]
+evalTotalPrimopCode (IntGcd      a b) = [LInteger (gcdInteger a b)]
+evalTotalPrimopCode (IntGcdExt   a b) = [LInteger g, LInteger s]
   where (# g, s #) = gcdExtInteger a b
-eval (RatAdd      a b) = [LRational (a + b)]
-eval (RatSub      a b) = [LRational (a - b)]
-eval (RatMult     a b) = [LRational (a * b)]
-eval (RatDiv      a b) = [LRational (a / b)]         -- UNDEFINED for b = 0
-eval (NatAdd      a b) = [LNatural (a + b)]
-eval (NatMult     a b) = [LNatural (a * b)]
+evalTotalPrimopCode (RatAdd      a b) = [LRational (a + b)]
+evalTotalPrimopCode (RatSub      a b) = [LRational (a - b)]
+evalTotalPrimopCode (RatMult     a b) = [LRational (a * b)]
+--evalTotalPrimopCode (RatDiv      a b) = [LRational (a / b)]         -- UNDEFINED for b = 0
+evalTotalPrimopCode (NatAdd      a b) = [LNatural (a + b)]
+evalTotalPrimopCode (NatMult     a b) = [LNatural (a * b)]
 -- NOTE: for nats, QuotRem and DivMod are identical
-eval (NatQuotRem a b) = let (q, r) = a `quotRem` b  -- UNDEFINED for b = 0
-                        in [LNatural q, LNatural r]
-eval (NatDivMod  a b) = let (d, m) = a `divMod` b   -- UNDEFINED for b = 0
-                        in [LNatural d, LNatural m]
-eval (NatPowMod  b e m) = [LNatural (powModNatural b e m)]
+--evalTotalPrimopCode (NatQuotRem a b) = let (q, r) = a `quotRem` b  -- UNDEFINED for b = 0
+--                        in [LNatural q, LNatural r]
+--evalTotalPrimopCode (NatDivMod  a b) = let (d, m) = a `divMod` b   -- UNDEFINED for b = 0
+--                        in [LNatural d, LNatural m]
+evalTotalPrimopCode (NatPowMod  b e m) = [LNatural (powModNatural b e m)]
