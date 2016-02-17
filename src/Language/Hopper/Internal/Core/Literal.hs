af36f17ad50eb52d1ff85eebf29b29053613f414
Brian Schroeder <bts@gmail.com>
2016-02-17 10:46:36 -0500

    Clean-up formatting

--- src/Language/Hopper/Internal/Core/Literal.hs
+++ src/Language/Hopper/Internal/Core/Literal.hs
@@ -1,23 +1,30 @@
 {-# LANGUAGE DeriveDataTypeable,DeriveGeneric #-}
-module Language.Hopper.Internal.Core.Literal(
-    Literal(..)
-    ,PrimOpId(..)
-    ,ConstrId(..)
-    )
-where
+module Language.Hopper.Internal.Core.Literal
+  ( Literal(..)
+  , PrimOpId(..)
+  , ConstrId(..)
+  ) where
 
 import Numeric.Natural
 import Data.Data
-import  Data.Text (Text)
+import Data.Text (Text)
 import GHC.Generics
 
-data Literal = LInteger !Integer | LRational !Rational | LNatural !Natural | LText !Text
-  deriving(Eq,Ord,Show,Read,Typeable,Data,Generic)
+data Literal
+  = LInteger !Integer
+  | LRational !Rational
+  | LNatural !Natural
+  | LText !Text
+  deriving (Eq,Ord,Show,Read,Typeable,Data,Generic)
 
 -- | ConstrId is the tag name for data type constructors, we dont have those yet
 -- likewise, if we want to contemplate type directed name resolution,
 -- we will need to do enriched name spacing!
-newtype ConstrId  = ConstrId { unConstrId :: Text } deriving (Eq, Show,Typeable,Ord,Read,Data,Generic)
+newtype ConstrId
+  = ConstrId { unConstrId :: Text }
+  deriving (Eq,Show,Typeable,Ord,Read,Data,Generic)
 
 -- Primops for a language have names
-newtype PrimOpId = PrimopId {unPrimopId :: Text}  deriving  (Eq, Show,Typeable,Ord,Read,Data,Generic)
+newtype PrimOpId
+  = PrimopId { unPrimopId :: Text }
+  deriving (Eq,Show,Typeable,Ord,Read,Data,Generic)
