f41831f99a143ede581338347bbe655e3fb0b494
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2016-02-29 23:41:27 -0500

    more notes towards the prismy tag dispatchy hooks for modularizing the runtime system instantiation

--- src/Hopper/Internal/Runtime/TaggedValueSelector.hs
+++ src/Hopper/Internal/Runtime/TaggedValueSelector.hs
@@ -2,6 +2,10 @@
 
 module Hopper.Internal.Runtime.TaggedValueSelector where
 
+
+
+
+
 {-
 this module provides the semi high level heap value tag checker /extractors
 
@@ -15,4 +19,9 @@ class LiteralSelector hval  neut  ref | hval -> neut ref where
   literalSelect :: forall s . hval -> STE HeapError s (Maybe (Either neut (LitF ref) ))
 
 or a similar prim monad esque  design
+-}
+
+{- |
+
+
 -}
