76ec8579ed7c27c9147f7ab657c16c614fd497ff
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2016-03-07 11:21:57 -0500

    move to locallynamless and split out some self contained pieces of the machinery

--- src/Hopper/Internal/Core/Term.hs
+++ src/Hopper/Internal/Core/Term.hs
@@ -5,154 +5,116 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-module Hopper.Internal.Core.Term where
+
+module  Hopper.Internal.Core.Term where
+
+
 
 import Hopper.Internal.Core.Literal
 -- import Hopper.Internal.Core.Type
-import Data.Text (Text)
+import Data.Text  as T (Text)
 import Data.Data
 --import Bound
-import Data.Bifunctor
-import Data.Word (Word64)
+--import Data.Bifunctor
+import Data.Word (Word32)
 --import Prelude.Extras
-import Control.Monad
 import GHC.Generics (Generic)
+import Hopper.Internal.Type.Relevance(Relevance)
+import Hopper.Utils.LocallyNameless
 --import Data.Traversable --  (fmapDefault,foldMapDefault)
-
-newtype Scope b {-(f :: * -> *)-} a
-  = Scope {unscope :: Term (Var b (Term a))}
-  deriving ({-Show1,Read1,Ord1,Eq1,-}Ord,Eq,Generic,Show,Read,Functor,Foldable,Typeable,Traversable)
-instance Bifunctor Scope  where
-  bimap f g  (Scope tm) = Scope $ fmap (bimap f (fmap g)) tm
+import qualified Data.Vector as V
 
 
-data Var b a = B b | F a
-  deriving ({-Show1,Read1,Ord1,Eq1,-}Ord,Eq,Data,Generic,Show,Read,Functor,Foldable,Typeable,Traversable)
-instance Bifunctor Var where
-  {-# INLINE bimap #-}
-  bimap = \ f g  v-> case v of B bv -> B $ f bv ; F fv -> F $ g fv
+type Type = () -- TODO FIXME, wire in the real type info?, which will be "Term" :)
 
-abstract ::  (a -> Maybe b) -> Term a -> Scope b  a
-abstract f e = Scope (liftM k e) where
-  k y = case f y of
-    Just z  -> B z
-    Nothing -> F (return y)
-instantiate ::  (b -> Term a) -> Scope b  a -> Term a
-instantiate k e = unscope e >>= \v -> case v of
-  B b -> k b
-  F a -> a
+data BinderInfo =
+      BinderInfoData
+        {relevanceBI :: Relevance -- if zero, during evaluation we need not pass around
+                                     --  BUT during NORMALIZATION, we probably do
+                                     --  so the normalizer WILL thread around irrelevant values
+                                     -- to properly support dependent type checking
+                                     -- as is proper, because a runtime irrelevant value
+                                     -- SHOULD be relevant during type checking, or
+                                     -- it has no reason to exist
+        , typeBICC :: Type -- at least for now, closure converted stuff may need a
+          -- slightly different type system fragment than the Core layer Terms?
+          -- NB: once we have existentials, should just be an "equivalent" subset
+          -- of the full type theory?
+        , sourceInfo :: Maybe Text --- this isn't quite right ...
+        -- also should add
+          } --- this needs to be fleshed out
+  deriving(Eq,Ord,Read,Show,Typeable,Data,Generic)
 
-
-gbind ::  (a -> Term c) -> Scope b a  -> Scope b c
-gbind f (Scope tm) = Scope $ fmap (fmap (>>= f))  tm
-
-{-
-
-class Bound (t :: (* -> *) -> * -> *) where
-  (>>>=) :: Monad f => t f a -> (a -> f c) -> t f c
-    Scope b a -> (a -> Term c) -> Scope b c
-    uses bind :: Term a -> (a -> Term C) -> Term c
-    so can do
-
--- >>> :m + Data.List
--- >>> abstract (`elemIndex` "bar") "barry"
--- Scope [B 0,B 1,B 2,B 2,F "y"]
-abstract :: Monad f => (a -> Maybe b) -> f a -> Scope b f a
-abstract f e = Scope (liftM k e) where
-  k y = case f y of
-    Just z  -> B z
-    Nothing -> F (return y)
-{-# INLINE abstract #-}
-
-
-
--- | Enter a scope, instantiating all bound variables
---
--- >>> :m + Data.List
--- >>> instantiate (\x -> [toEnum (97 + x)]) $ abstract (`elemIndex` "bar") "barry"
--- "abccy"
-instantiate :: Monad f => (b -> f a) -> Scope b f a -> f a
-instantiate k e = unscope e >>= \v -> case v of
-  B b -> k b
-  F a -> a
-{-# INLINE instantiate #-}
-
--- | Enter a 'Scope' that binds one variable, instantiating it
---
--- >>> instantiate1 "x" $ Scope [B (),F "y",F "z"]
--- "xyz"
-instantiate1 :: Monad f => f a -> Scope n f a -> f a
-instantiate1 e = instantiate (const e)
-{-# INLINE instantiate1 #-}
-
-
--}
-
-data Term a
-  = V  !a
+data Term =
+  V  Variable
+  | BinderLevelShiftUP Word32 Term  --
   | ELit !Literal
-  | Return ![ Term a ] -- explicit multiple return values
+  | Return !(V.Vector Term)  -- explicit multiple return values
                       -- should V x be replaced by Return [x] ?
                       --  once we lower to ANF
                       -- NOTE: for valid expressions,
-  | EnterThunk !(Term a) -- because we're in a strict IR rep,
+  | EnterThunk !(Term) -- because we're in a strict IR rep,
                         -- we dont need to provide a seq like operation
                           -- seq a b === let _ := enterThunk a in b
 
-  | Delay !(Term a)  --- Delay is a Noop on Thunked values, otherwise creates a Thunked
+  | Delay !(Term )  --- Delay is a Noop on Thunked values, otherwise creates a Thunked
                     --- note: may need to change their semantics later?!
                     --- Q: is it valid to thunk a thunked value? (no?)
-  | App !(Term  a)  ![Term  a]  --this is not curried :)
+  | App !(Term  )  !(V.Vector Term  )   --this is not curried :)
   | PrimApp  !PrimOpId --
-             ![Term  a] -- not sure if this is needed, but lets go with it for now
+             !(V.Vector Term  ) -- not sure if this is needed, but lets go with it for now
 
-  | Lam ![(Text{-,Type ty,RigModel-})]
-        !(Scope Text  a)
-  | Let !(Either Word64  [Text]) -- either the # of multiple RVs from the rhs,
-                                  -- or the names for the values on the RHS
-      --  this was the optional type annotation? (Maybe  () {-(Type ty,RigModel)-})
-          !(Term  a)   -- rhs which may have multi verses
-          (Scope (Either Word64 Text)   a) --  [Scope Int Term a] (Scope Int Term a)
-  deriving ({-Show1,Read1,Ord1,Eq1,-}Ord,Eq,Show,Read,Functor,Foldable,Typeable,Traversable)
+  | Lam !(V.Vector BinderInfo)
+         !Term
+  | Let !(V.Vector BinderInfo)
+           Term --- RHS
+           Term --- BODY
+  deriving ({-Show1,Read1,Ord1,Eq1,-}Ord,Eq,Show,Read{-,Functor,Foldable-},Typeable{-,Traversable-})
 
 
--- instance Functor (Term ty)  where fmap       = fmapDefault
+--- NOTE: USE STE MONAD ONCE WE MIGRATE TO HSUM DESIGN
+--- this is kinda only for "inlining" on debruijin variables for now
+substitute :: Word32 -> (BinderSlot  -> Maybe Term) -> Term -> Either (String,Word32) Term
+substitute baseLevel initMapper initTerm = goSub 0 initMapper initTerm
+  where
+    goSub :: Word32 -> (BinderSlot  -> Maybe Term) -> Term -> Either (String,Word32) Term
+    goSub shift mapper  var@(V (LocalVar (LocalNamelessVar lnLvl bslt@(BinderSlot lnSlot))))
+                |  lnLvl == (shift + baseLevel) =
+                        maybe (Left ("bad slot", lnSlot))
+                              (Right . BinderLevelShiftUP shift )
+                             $ mapper bslt
+                | otherwise =  Right var
+    goSub _l _m var@(V (GlobalVarSym _ )) = Right var
+    goSub shift mapper (Return ls) =  fmap Return $  mapM (goSub shift mapper ) ls
+    goSub shift mapper (App fun args) =
+          do   funNew <- goSub shift mapper fun
+               argsNew <-  mapM (goSub shift mapper) args
+               Right (App funNew argsNew)
+    goSub shift mapper (Lam binders bod) =
+          do   bodNew <- goSub (shift +1 )  mapper bod ; Right (Lam binders bodNew)
+    goSub shift mapper (Let binders rhs bod) =
+          do  rhsNew <- goSub shift mapper rhs ;
+              bodNew <- goSub (shift +1 ) mapper bod ;
+              Right (Let binders rhsNew bodNew)
+    goSub shift mapper (PrimApp primop args) =
+          do  argsNew <- mapM (goSub shift mapper) args
+              Right (PrimApp primop argsNew)
+    goSub shift mapper (BinderLevelShiftUP posAmt bod)
+      | posAmt <= baseLevel + shift  = -- TODO AUDIT THIS TO TILL IT SCREAMS
+          do  bodNew <- substitute (baseLevel + shift - posAmt) mapper bod
+              Right (BinderLevelShiftUP posAmt bodNew)
+      | otherwise = Left ("weird level shift appeared, you've been eaten by a grue", posAmt )
+                --- OR WHATTTTTT??!?!?!?!
+    goSub _shift _mapper lit@(ELit _)  = Right lit
+    goSub shift mapper (EnterThunk bod ) =  fmap EnterThunk $ goSub shift mapper bod
+    goSub shift mapper (Delay bod ) = fmap Delay $ goSub shift mapper bod
 
--- instance Foldable (Term ty) where foldMap    = foldMapDefault
-
-instance Applicative (Term ) where
-  pure  = V
-  (<*>) = ap
-
--- instance Traversable (Term ty) where
---   traverse f (V a)      = V <$> f a
---   traverse _f (ELit e) = pure $ ELit e
---   -- traverse f (PrimApp nm ls) = PrimApp nm <$> traverse f ls
---   traverse f (Force e) = Force <$> traverse f e
---   traverse f (Delay e) = Delay <$> traverse f e
---   traverse f (PrimApp nm args) = PrimApp nm <$> traverse (traverse f) args
---   traverse f (x :@ ys)   = (:@) <$> traverse f x <*> traverse (traverse f) ys
---   traverse f (Lam t e)    = Lam  t <$> traverse f e
---   traverse f (Let mname mtype  bs b) = Let  mname mtype <$>  (traverse f) bs <*> traverse f b
 
 
-instance Monad (Term ) where
-  return = V
-  V a         >>= f = f a
-  (Return ls) >>= f =    Return (map (>>= f) ls )
-  -- PrimApp nm ls >>= f = PrimApp nm (map f ls)
-  Delay e     >>= f = Delay $ e >>= f
-  EnterThunk thnk     >>= f =  EnterThunk (thnk >>= f)
-  ELit e      >>= _f = ELit e -- this could also safely be a coerce?
-  (App x y)    >>= f = App (x >>= f)  (map (>>= f)  y )
-  (PrimApp name args) >>= f = PrimApp name (map (>>= f) args )
-  Lam t  e    >>= f =   Lam t (gbind f e)
-  Let mname {-mtype-} bs  b >>= f = Let mname {-mtype-} (  bs >>= f)  (gbind f b)
+{-
+            _ _ (BinderLevelShiftUP _ _)
+            _ _ (ELit _)
+            _ _ (EnterThunk _)
+            _ _ (Delay _)
 
-
-
-
-callPrim :: (Either Text GmpMathOpId) -> [Term  a] -> Term  a
-callPrim (Left name) = PrimApp  (PrimopIdGeneral name)
-callPrim (Right gmpid) = PrimApp (TotalMapthOpGmp gmpid)
-
+-}
---
+++ src/Hopper/Utils/LocallyNameless.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable,DeriveAnyClass #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+
+
+module Hopper.Utils.LocallyNameless where
+
+
+import Data.Word
+import Data.Data
+import GHC.Generics
+import Hopper.Internal.Type.Relevance
+import qualified Data.Text as T (Text )
+
+--- | GlobalSymbol should correspond to the fully qualified name
+--- of a reachable value that is induced UNIQUELY by a module's name and
+--- set of dependencies and how it was built.
+--- NB: this might be more subtle in the presence of linearity, but lets table that for now
+---
+--- this may or may not  actually need to just be a functory parametery in the AST
+--- but lets keep it simple fo rnow
+newtype GlobalSymbol = GlobalSymbol T.Text
+  deriving (Eq,Ord,Read,Show,Data,Typeable,Generic)
+
+
+newtype BinderSlot =
+    BinderSlot { unBinderSlot :: Word32 }
+  deriving (Eq, Show,Data,Ord,Read,Typeable,Generic)
+
+data LocalNamelessVar =
+   LocalNamelessVar
+          {localBinderDepth :: {-# UNPACK #-}  !Word32
+           ,localBinderArg :: {-# UNPACK #-}   !BinderSlot
+         }
+  deriving(Eq,Ord,Read,Show,Typeable,Data,Generic )
+
+-- | VariableCC is either a local env variable or a globally fixed symbol (think like linkers and object code)
+-- TODO: later lowering passes will induce register / stack placements and
+-- veer into forcing specification of caller/callee saving conventions on code control tranfers
+data Variable  =
+    LocalVar {-# UNPACK #-} !LocalNamelessVar
+    | GlobalVarSym {-# UNPACK #-}  !GlobalSymbol
+  deriving(Eq,Ord,Read,Show,Typeable,Data,Generic )
+
