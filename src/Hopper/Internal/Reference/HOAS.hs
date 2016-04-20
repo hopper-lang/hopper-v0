08f6526b70380975a9b94d6497636b2f19d0dc23
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2016-04-20 17:52:57 -0400

    Got the basic encoding of Function spaces being both dependent sigma and pi telescopes at the same time!

--- src/Hopper/Internal/Reference/HOAS.hs
+++ src/Hopper/Internal/Reference/HOAS.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE ConstraintKinds   #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 --
 -- {-# LANGUAGE TypeInType #-}
@@ -10,11 +11,14 @@
 module Hopper.Internal.Reference.HOAS where
 
 import qualified GHC.TypeLits as GT
-import GHC.TypeLits (Nat)
+import Data.Primitive.MutVar
+--import Control.Monad.Primitive
+import GHC.TypeLits (Nat,KnownNat)
 import GHC.Types (Type, Constraint) -- this isn't 7.10 compatible but its sooo nice :)
 import Data.Text (Text)
 import Data.Void
 import Data.Proxy
+import Hopper.Internal.Type.Relevance
 
 {- A Higher Order  abstract syntax model of the term AST
 There will be a few infelicities to simplify / leverage the use
@@ -32,11 +36,11 @@ data SizedTelescope :: Nat -> (Type ) ->(Nat -> Type -> Type ) -> (Nat -> Constr
 
 
 data ValFun :: Nat -> Type ->  Type where
-  ZeroVF :: (() -> [Value neut]) -> ValFun 0 neut
-  SucVF :: {-GT.KnownNat n =>-} (Value neut -> ValFun n neut ) -> ValFun (n GT.+ 1) neut
+  ZeroVF :: (() -> a) -> ValFun 0 a
+  SucVF :: {-GT.KnownNat n =>-} (a -> ValFun n a ) -> ValFun (n GT.+ 1) a
 
 data SomeValFun :: Type ->  Type where
-  SomeValFun :: GT.KnownNat n => Proxy n -> ValFun n neut -> SomeValFun neut
+  SomeValFun :: GT.KnownNat n => Proxy n -> ValFun n a -> SomeValFun a
 
 
 --data ValFun :: Nat  One (Value -> [Value ])
@@ -44,11 +48,11 @@ data SomeValFun :: Type ->  Type where
 
 -- need to add relevance annotations to these
 data ExpFun :: Nat -> Type ->  Type where
-  Z :: ([Exp a]) -> ExpFun 0 a
+  Z :: (Exp a) -> ExpFun 0 a
   S :: (a -> ExpFun n a) -> ExpFun (n GT.+ 1) a
 
 data SomeExpFun :: Type -> Type where
-  SomeExpFun :: GT.KnownNat n => Proxy n -> ExpFun  n neut -> SomeExpFun neut
+  SomeExpFun :: GT.KnownNat n => Proxy n -> ExpFun  n a -> SomeExpFun a
 
 data Literal :: Type  where --- this lives in a nother module, but leave empty for now
  LInteger :: Integer -> Literal
@@ -57,14 +61,31 @@ data Literal :: Type  where --- this lives in a nother module, but leave empty f
 data DataDesc
 
 
-data Value :: Type  -> Type where
-  VLit :: Literal -> Value neut
-  VFunction :: GT.KnownNat n => ValFun n neut -> Value neut
-  VConstructor :: Text -> [Value neut ] -> Value neut
-  VNeutral :: neut -> Value neut
-  VThunk :: Thunk neut -> Value neut
+-- This factorization is to require
+data ValueNoThunk :: Type -> (Type -> Type )  -> Type where
+  VLit :: Literal ->  ValueNoThunk s neut
+  VFunction :: (SomeValFun (Value s neut )) -> ValueNoThunk s neut
+  VConstructor :: Text -> [Value s neut ] -> ValueNoThunk s  neut
+  VNeutral :: neut s -> ValueNoThunk s neut
 
-data Thunk :: Type -> Type where
+{-
+TODO: add normalized types
+
+-}
+
+--- Values are either in Normal form, or Neutral, or a Thunk
+data Value :: Type -> (Type -> Type )  -> Type where
+    VNormal :: ValueNoThunk s neut -> Value  s neut
+    VThunk :: Thunk s neut -> Value s neut
+
+
+data ThunkValuation :: Type -> (Type -> Type ) -> Type where
+  ThunkValueResult :: (ValueNoThunk s neut) ->  ThunkValuation s neut
+  ThunkComputation :: (Exp (Value s neut)) -> ThunkValuation s neut
+  --- Q: should there be blackholes?
+
+data Thunk :: Type -> (Type -> Type ) -> Type where
+  Thunk :: MutVar s (ThunkValuation s neut) -> Thunk s neut
 --- figure this out, or maybe values need to be ST branded?
 
 --- this isn't quite right yet
@@ -86,19 +107,112 @@ data Sort :: Type   where
 data PrimType :: Type where
   PTInteger :: PrimType
 
-data Exp a where
+--- this is in some sense
+data Telescope :: (Nat -> Type ->  Type -> Type) -> Type -> Nat -> Type where
+  TZ :: f 0 t  t -> Telescope f  t 0
+  TSucc :: forall t f  (n :: Nat) (m :: Nat) . (m ~ (n GT.+ 1)) =>
+              (f m  t (Telescope f t n)) -> Telescope f t m
 
-  -- this model of pi and sigma aren't quite right
-  -- think about it more tomorrow
-  Pi :: SomeExpFun a -> Exp a
-  Sigma :: Exp a -> SomeExpFun a -> Exp a
+{- is this a profunctor? or something nuts? or both :))
+   its like some sort of categorical thingy
+   point being, its meant to model dependent pi
+   ie
+   Pi {x_1 : }
+ -}
+data PiTel :: Nat -> Type -> Type  -> Type -> Type where
+  PiZ :: forall domainV domTy codomainTy .
+       codomainTy ->
+       -- ^ a Zero arg function is logically just the bare expression,
+       -- merely unevaluated. that is, PiZ (Exp a) is the same as
+       -- a unit value argument function  @ ()-> codomain @
+       PiTel 0 domainV domTy codomainTy
+
+  PiSucc :: forall domainV domTy codomainV m n . (m ~ (n GT.+ 1)) =>
+          domTy ->
+          -- ^ type of domain / current variable
+          Relevance ->
+          -- ^ variable usage annotation for the thusly typed function expression
+          -- usage in type level expressions is deemed cost 0
+          (domainV -> PiTel n domainV domTy codomainV ) ->
+          -- ^ rest of the telescope
+          PiTel m domainV domTy codomainV
+
+
+data SigmaTel :: Nat -> Type -> Type -> Type where
+  SigmaZ :: forall domainV domTy . SigmaTel 0 domainV domTy
+  -- ^ an empty sigma telescope is basically just the unit value
+  SigmaSucc :: forall domainV domTy m n . (m ~ (n GT.+ 1)) =>
+            domTy ->
+            -- ^ the type of the first element
+            domainV ->
+            -- ^ sigmas are pairs! so we have the "value" of the first element :)
+            Relevance ->
+            -- ^ the computational relevance for the associated value
+            -- usage in type level expressions is deemed cost 0
+            (domainV -> SigmaTel n domainV domTy) ->
+            -- ^ second/rest of the telescope
+            -- (sigmas are, after a generalized pair)
+            SigmaTel m domainV domTy
+
+data SomeNatF :: (Nat -> Type) -> Type where
+  SomeNatF ::  forall n f . GT.KnownNat n => f n -> SomeNatF f
+
+
+--data HoasType :: (Type -> Type ) where
+--   --FunctionSpace ::
+
+
+
+data Exp :: Type -> Type  where
+
+  {-
+  our function type from unboxed tuples arity n to unboxed tuples arity m
+  should model the following coinductive / inductive type
+  forall*{x1  :t1 ... xn :tn  } -> exist*{r1: q1 .. rm : qm}
+
+  sigma* {r1: q1 .. rm : qm} === forall {} -> exist*{r1: q1 .. rm : qm}
+  -}
+  FunctionSpaceExp :: (KnownNat piSize, KnownNat sigSize) =>
+      Proxy piSize ->
+      Proxy sigSize ->
+      (PiTel piSize a (Exp a)
+        (SigmaTel sigSize a (Exp a))) ->
+      -- ^ the notion of unboxed tuple telescopes
+      -- in both argument and result positions (surprisingly)
+      -- results in an interesting unification of dependent sums and products
+      -- which also lends itself to some pretty cool logical embeddings!
+      -- Eg roughly @ Void === pi{a : Type}->sigma{res : a} @ which has zero
+      -- inhabitants,
+      -- and likewise something like  @ Unit === pi{ a : Type, v : }-> Sigma{}@
+      -- or perhaps @  Unit == pi{}->sigma{} @, as either of those types
+      -- have only one inhabitant!
+      Exp a
   BaseType :: PrimType -> Exp a
+  --ExpType :: HoasType (Exp a) -> Exp a
   Sorts :: Sort  -> Exp a
   Abs :: SomeExpFun a -> Exp a
   App :: Exp a -> Exp a -> Exp a
-  Val :: a -> Exp a  --- values or names etc ???
+  EReturn :: [Exp a] -> Exp a
+  HasType :: Exp a -> Exp a -> Exp a  --- aka CUT
+  {- TODO ADD LET -}
+  Var :: a -> Exp a  --- values or names etc ???
   Case :: Exp a -- ^ the value being cased upon
-        -> Maybe (Exp a)  -- optional type annotation
+        -> Maybe (Exp a)  -- optional type annotation,
+                          -- also Joel thinks this is smelly, perhaps rightly
         -> [(Text, SomeExpFun a )] -- non-overlapping set of tags and continuations
         -> Exp a
+{-
+queue
+
+first order -> hoas feasiblity sanity check
+normal forms on types (values is bigger than we knew!)
+BIDIRECTIONAL CHECKING (syntax) guillaume allais etc
+make the Function space stuff not suck
+
+-}
+
+
+evalB :: Exp a -> a
+evalB  _ = undefined
+
 
